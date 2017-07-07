89b91bdca9ad2fccb6babf2485a0281c511897fa
Tyler Stephenson <ilduchea@gmail.com>
2017-07-07 14:27:18 -0700

    add review delete functionality for admins

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,12 +3,24 @@ class ReviewsController < ApplicationController
   def create
     @product = Product.find(params[:product_id])
     @review = @product.reviews.create review_params
-    @review.save
-    redirect_to (request.env['HTTP_REFERER']), notice: "Your review was successfully posted."
+    @review.author = current_user.name
+    if @review.save
+      flash[:notice] = "Your review was successfully posted."
+      redirect_to (request.env['HTTP_REFERER'])
+    end
+
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    if @review.destroy
+      flash[:notice] = "Review successfully removed!"
+      redirect_to request.env['HTTP_REFERER']
+    end
   end
 
   private
     def review_params
-      params.require(:review).permit(:author, :body)
+      params.require(:review).permit(:body)
     end
 end
