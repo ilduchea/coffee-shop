dec45ee7bae5d273868d91609f0c08a27e79e4d2
Tyler Stephenson <ilduchea@gmail.com>
2017-07-07 11:16:03 -0700

    add is_admin helper. add db seeding

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user
+  helper_method :current_user, :is_admin?
 
   def current_user
     if session[:user_id]
       @current_user = User.find(session[:user_id])
     end
+  end
+
+  def is_admin?
+    current_user && current_user.admin
   end
 
   def authorize
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+Review.destroy_all
+User.destroy_all
+
+1.times do |index1|
+  User.create!(
+    name: 'Qwerty',
+    email: 'qwerty@email.com',
+    password: 'password',
+    admin: true
+  )
+end
+
+50.times do |index1|
+  myProduct = Product.create!(
+    name: Faker::Coffee.blend_name,
+    cost: Faker::Number.between(1, 50),
+    origin: Faker::Coffee.origin,
+    description: Faker::Coffee.notes
+  )
+  5.times do |index2|
+    myProduct.reviews.create!(
+    author: Faker::HitchhikersGuideToTheGalaxy.character,
+    body: Faker::HitchhikersGuideToTheGalaxy.quote
+  )
+  end
+end
+
+p "Created #{User.count} users"
+p "Created #{Product.count} products"
+p "Created #{Review.count} reviews"
